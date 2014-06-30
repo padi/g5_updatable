441d94c101b0cb745365c3c8d6cc8f38d69e01de
bbauer <brian.bauer@g5platform.com>
2014-06-30 13:50:14 -0700

    Add ruby version

--- app/controllers/g5_updatable/feed_controller.rb
+++ app/controllers/g5_updatable/feed_controller.rb
@@ -1,6 +1,5 @@
 class G5Updatable::FeedController < ApplicationController
   def update
-    #Resque.enqueue(ClientUpdaterJob)
     G5Updatable::ClientFeedProcessor.work
     render json: {}, status: :ok
   end
